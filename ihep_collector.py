3ebc208f6ee9827db21ba2577f4e8fcb9d6c97d6
mekuun <104074737+mekuun@users.noreply.github.com>
2025-05-17 12:01:26 +0300

    Update ihep_collector.py

--- ihep_collector.py
+++ ihep_collector.py
@@ -1,69 +1,39 @@
 import requests
-import pandas as pd
-from datetime import datetime
-import logging
+import csv
 
-logging.basicConfig()
+conferences_url = 'https://inspirehep.net/api/conferences'
+query = 'opening_date:[2015-01-01 TO 2024-12-31]'
+params = {
+    'q': query,
+    'size': 1000,
+}
 
-conf_notes_url = 'https://inspirehep.net/api/literature?size=1000&fields=publication_info.conference_record,publication_info.cnum&doc_type=conference%20paper&collaboration=ATLAS&page=1&q=&earliest_date=2021--2024'
+response = requests.get(conferences_url, params=params)
+data = response.json()
 
+conference_list = []
+for conference in data['hits']['hits']:
+    conference_metadata = conference['metadata']
+    conference_id = conference_metadata['control_number']
+    conference_name = conference_metadata.get('titles', [{}])[0].get('title', 'No Title')
+    conference_cnum = conference_metadata['cnum']
+    number_of_contributions = conference_metadata.get('number_of_contributions', 0)
 
-def get_atlas_conferences():
-    """Собирает список конференций и количество публикаций для каждой, сохраняет в CSV."""
-    conf_notes = requests.get(conf_notes_url).json()
-    root = conf_notes['hits']['hits']
+    opening_date = conference_metadata.get('opening_date', 'Unknown')
+    year = opening_date.split('-')[0] if opening_date != 'Unknown' else 'Unknown'
 
-    conferences = []
-    for i in root:
-        if 'metadata' in i and 'publication_info' in i['metadata']:
-            pub_info = i['metadata']['publication_info'][0]
-            if 'conference_record' in pub_info:
-                url = pub_info['conference_record']['$ref']
-                existing_conf = next((c for c in conferences if c['url'] == url), None)
+    conference_list.append({
+        'id': conference_id,
+        'name': conference_name,
+        'year': year,
+        'number_of_contributions': number_of_contributions,
+        'cnum': conference_cnum
+    })
 
-                if not existing_conf:
-                    conferences.append({'url': url, 'n_papers': 1})
-                else:
-                    existing_conf['n_papers'] += 1
+sorted_conferences = sorted(conference_list, key=lambda x: x['number_of_contributions'], reverse=True)
 
-    df = pd.DataFrame(conferences)
-    df.to_csv('atlas_conferences.csv', index=False)
-    print("Конференции сохранены в 'atlas_conferences.csv'")
-
-
-def collect_conference_details():
-    """Собирает детальную информацию по каждой конференции из списка и сохраняет в CSV."""
-    conf_notes = requests.get(conf_notes_url).json()
-    root = conf_notes['hits']['hits']
-
-    conferences = []
-    for i in root:
-        if 'metadata' in i and 'publication_info' in i['metadata']:
-            pub_info = i['metadata']['publication_info'][0]
-            if 'conference_record' in pub_info:
-                url = pub_info['conference_record']['$ref']
-                existing_conf = next((c for c in conferences if c['url'] == url), None)
-
-                if not existing_conf:
-                    conferences.append({'url': url, 'n_papers': 1})
-                else:
-                    existing_conf['n_papers'] += 1
-
-    for conf in conferences:
-        data = requests.get(conf['url']).json()
-        conf['series'] = data['metadata'].get('series', [{}])[0].get('name', '')
-        conf['number'] = data['metadata'].get('series', [{}])[0].get('number', '')
-        conf['title'] = data['metadata'].get('titles', [{}])[0].get('title', '')
-        conf['opening_date'] = data['metadata'].get('opening_date', '')
-        conf['closing_date'] = data['metadata'].get('closing_date', '')
-        conf['acronym'] = data['metadata'].get('acronyms', [''])[0]
-
-    df = pd.DataFrame(conferences)
-
-    date_columns = ['opening_date', 'closing_date']
-    for col in date_columns:
-        df[col] = pd.to_datetime(df[col], errors='coerce')
-
-    df.to_csv('ihep_conf_details.csv', index=False)
-    print("Детали конференций сохранены в 'ihep_conf_details.csv'")
-
+csv_filepath = "conferences_data_with_year.csv"
+with open(csv_filepath, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=["id", "name", "year", "number_of_contributions", 'cnum'])
+    writer.writeheader()
+    writer.writerows(sorted_conferences)
